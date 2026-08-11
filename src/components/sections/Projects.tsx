49d1d23ac2c151ab66f6f4fb4fc5107d5bfeea8e
Raul <raulrangel1230@gmail.com>
2026-08-11 11:48:55 -0400

    add nexusdoc project

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -5,10 +5,18 @@ import ProjectCard from '@/components/cards/ProjectCard'
 
 const projects = [
   {
+    title: 'NexusDoc',
+    description:
+      'Sistema web de citas medicas, limpio, moderno y con diseño intuitivo para una experiencia de usuario fluida.',
+    media: '/nexusdoc.webp',
+    technologies: ['PHP', 'Laravel', 'Tailwind'],
+    liveUrl: 'https://nexus-doc.onrender.com/',
+  },
+  {
     title: 'Dorta',
     description:
       'Pagina web de una empresa de ferretería, con diseño moderno y animaciones suaves para una experiencia atractiva.',
-    media: '/dorta.png',
+    media: '/dorta.webp',
     technologies: ['NextJs', 'TypeScript', 'Tailwind', 'Framer Motion'],
     liveUrl: 'https://dorta.vercel.app/',
   },
@@ -16,7 +24,7 @@ const projects = [
     title: 'Portfolio Website',
     description:
       'Portafolio moderno desarrollado con Next.js, animaciones fluidas y diseño High-Tech y Espacial.',
-    media: '/cunaguaros-page.png',
+    media: '/cunaguaros-page.webp',
     technologies: ['Next.js', 'TypeScript', 'Tailwind', 'Framer Motion'],
     liveUrl: 'https://portfolio-cunaguarosdev.vercel.app/',
   },
@@ -29,16 +37,14 @@ export default function Projects() {
 
       <section
         id='projects'
-        className='relative py-24 px-6 md:px-16 bg-neutral-950 overflow-hidden'
-      >
+        className='relative py-24 px-6 md:px-16 bg-neutral-950 overflow-hidden'>
         <div className='relative max-w-6xl mx-auto'>
           <div className='flex flex-row items-start'>
             <motion.h2
               initial={{ opacity: 0, y: 40 }}
               whileInView={{ opacity: 1, y: 0 }}
               viewport={{ amount: 0.5, once: true }}
-              className='text-4xl md:text-5xl font-extrabold orbitron mb-4 md:mb-8 bg-linear-to-r from-cyan-500 to-fuchsia-500 bg-clip-text text-transparent select-none min-w-min'
-            >
+              className='text-4xl md:text-5xl font-extrabold orbitron mb-4 md:mb-8 bg-linear-to-r from-cyan-500 to-fuchsia-500 bg-clip-text text-transparent select-none min-w-min'>
               Projects
             </motion.h2>
           </div>
@@ -48,8 +54,7 @@ export default function Projects() {
             whileInView='visible'
             viewport={{ once: true }}
             transition={{ staggerChildren: 0.15 }}
-            className='flex flex-wrap justify-center gap-8'
-          >
+            className='flex flex-wrap justify-center gap-8'>
             {projects.map((project, index) => (
               <motion.div
                 key={index}
@@ -57,8 +62,7 @@ export default function Projects() {
                   hidden: { opacity: 0, y: 40 },
                   visible: { opacity: 1, y: 0 },
                 }}
-                transition={{ duration: 0.5 }}
-              >
+                transition={{ duration: 0.5 }}>
                 <ProjectCard {...project} />
               </motion.div>
             ))}
